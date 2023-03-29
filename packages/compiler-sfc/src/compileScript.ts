joyfulmagician/vuejs-core

commit 5ca13b2cdbbbd5e4ab98b3153938d09cb4957114

fix(compiler-sfc): avoid codegen conflict with user variable named `expose` (#7949)
rename `expose` to `__expose` to avoid duplicate identifier

relate #7890

packages/compiler-sfc/src/compileScript.ts
@@ -1545,7 +1545,7 @@ export function compileScript(
   }
 
   const destructureElements =
-    hasDefineExposeCall || !options.inlineTemplate ? [`expose`] : []
+    hasDefineExposeCall || !options.inlineTemplate ? [`expose: __expose`] : []
   if (emitIdentifier) {
     destructureElements.push(
       emitIdentifier === `emit` ? `emit` : `emit: ${emitIdentifier}`
@@ -1723,7 +1723,7 @@ export function compileScript(
   // <script setup> components are closed by default. If the user did not
   // explicitly call `defineExpose`, call expose() with no args.
   const exposeCall =
-    hasDefineExposeCall || options.inlineTemplate ? `` : `  expose();\n`
+    hasDefineExposeCall || options.inlineTemplate ? `` : `  __expose();\n`
   // wrap setup code with function.
   if (isTS) {
     // for TS, make sure the exported type is still valid type with

packages/compiler-sfc/__tests__/compileScript.spec.ts
@@ -89,7 +89,7 @@ const bar = 1
     // should remove defineOptions import and call
     expect(content).not.toMatch('defineProps')
     // should generate correct setup signature
-    expect(content).toMatch(`setup(__props, { expose }) {`)
+    expect(content).toMatch(`setup(__props, { expose: __expose }) {`)
     // should assign user identifier to it
     expect(content).toMatch(`const props = __props`)
     // should include context options in default export
@@ -136,7 +136,9 @@ const myEmit = defineEmits(['foo', 'bar'])
     // should remove defineOptions import and call
     expect(content).not.toMatch('defineEmits')
     // should generate correct setup signature
-    expect(content).toMatch(`setup(__props, { expose, emit: myEmit }) {`)
+    expect(content).toMatch(
+      `setup(__props, { expose: __expose, emit: myEmit }) {`
+    )
     // should include context options in default export
     expect(content).toMatch(`export default {
   emits: ['foo', 'bar'],`)
@@ -272,7 +274,7 @@ defineExpose({ foo: 123 })
     // should remove defineOptions import and call
     expect(content).not.toMatch('defineExpose')
     // should generate correct setup signature
-    expect(content).toMatch(`setup(__props, { expose }) {`)
+    expect(content).toMatch(`setup(__props, { expose: __expose }) {`)
     // should replace callee
     expect(content).toMatch(/\bexpose\(\{ foo: 123 \}\)/)
   })
@@ -710,7 +712,7 @@ defineExpose({ foo: 123 })
         { inlineTemplate: true }
       )
       assertCode(content)
-      expect(content).toMatch(`setup(__props, { expose })`)
+      expect(content).toMatch(`setup(__props, { expose: __expose })`)
       expect(content).toMatch(`expose({ count })`)
     })
 
@@ -988,7 +990,7 @@ const emit = defineEmits(['a', 'b'])
       expect(content).toMatch(`export default /*#__PURE__*/_defineComponent({
   props: { foo: String },
   emits: ['a', 'b'],
-  setup(__props, { expose, emit }) {`)
+  setup(__props, { expose: __expose, emit }) {`)
     })
 
     test('defineProps w/ type', () => {
@@ -1524,7 +1526,7 @@ const emit = defineEmits(['a', 'b'])
       </script>
       `)
       assertCode(content)
-      expect(content).toMatch(`setup(__props, { expose, emit }) {`)
+      expect(content).toMatch(`setup(__props, { expose: __expose, emit }) {`)
       expect(content).toMatch(`emits: ['foo']`)
     })
 

packages/compiler-sfc/__tests__/__snapshots__/compileScriptRefTransform.spec.ts.snap
@@ -4,8 +4,8 @@ exports[`sfc ref transform > $ unwrapping 1`] = `
 "import { ref, shallowRef } from 'vue'
     
 export default {
-  setup(__props, { expose }) {
-  expose();
+  setup(__props, { expose: __expose }) {
+  __expose();
 
     let foo = (ref())
     let a = (ref(1))
@@ -25,8 +25,8 @@ exports[`sfc ref transform > $ref & $shallowRef declarations 1`] = `
 "import { ref as _ref, shallowRef as _shallowRef } from 'vue'
 
 export default {
-  setup(__props, { expose }) {
-  expose();
+  setup(__props, { expose: __expose }) {
+  __expose();
 
     let foo = _ref()
     let a = _ref(1)
@@ -46,8 +46,8 @@ exports[`sfc ref transform > usage /w typescript 1`] = `
 "import { ref as _ref, defineComponent as _defineComponent } from 'vue'
 
 export default /*#__PURE__*/_defineComponent({
-  setup(__props, { expose }) {
-  expose();
+  setup(__props, { expose: __expose }) {
+  __expose();
 
         let msg = _ref<string | number>('foo');
         let bar = _ref <string | number>('bar');
@@ -77,8 +77,8 @@ exports[`sfc ref transform > usage with normal <script> (has macro usage) + <scr
     let data = _ref()
     
 export default {
-  setup(__props, { expose }) {
-  expose();
+  setup(__props, { expose: __expose }) {
+  __expose();
 
     console.log(data.value)
     
@@ -95,8 +95,8 @@ exports[`sfc ref transform > usage with normal <script> + <script setup> 1`] = `
     let c = _ref(0)
     
 export default {
-  setup(__props, { expose }) {
-  expose();
+  setup(__props, { expose: __expose }) {
+  __expose();
 
     let b = _ref(0)
     let c = 0

packages/compiler-sfc/__tests__/__snapshots__/cssVars.spec.ts.snap
@@ -53,8 +53,8 @@ exports[`CSS vars injection > codegen > should ignore comments 1`] = `
 "import { useCssVars as _useCssVars, unref as _unref } from 'vue'
 const color = 'red';const width = 100
 export default {
-  setup(__props, { expose }) {
-  expose();
+  setup(__props, { expose: __expose }) {
+  __expose();
 
 _useCssVars(_ctx => ({
   \\"xxxxxxxx-width\\": (width)
@@ -70,8 +70,8 @@ exports[`CSS vars injection > codegen > should work with w/ complex expression 1
 "import { useCssVars as _useCssVars, unref as _unref } from 'vue'
 
 export default {
-  setup(__props, { expose }) {
-  expose();
+  setup(__props, { expose: __expose }) {
+  __expose();
 
 _useCssVars(_ctx => ({
   \\"xxxxxxxx-foo\\": (_unref(foo)),
@@ -94,8 +94,8 @@ exports[`CSS vars injection > codegen > w/ <script setup> 1`] = `
 "import { useCssVars as _useCssVars, unref as _unref } from 'vue'
 const color = 'red'
 export default {
-  setup(__props, { expose }) {
-  expose();
+  setup(__props, { expose: __expose }) {
+  __expose();
 
 _useCssVars(_ctx => ({
   \\"xxxxxxxx-color\\": (color)
@@ -112,8 +112,8 @@ exports[`CSS vars injection > codegen > w/ <script setup> using the same var mul
 const color = 'red'
         
 export default {
-  setup(__props, { expose }) {
-  expose();
+  setup(__props, { expose: __expose }) {
+  __expose();
 
 _useCssVars(_ctx => ({
   \\"xxxxxxxx-color\\": (color)
@@ -152,8 +152,8 @@ export default {
   props: {
           foo: String
         },
-  setup(__props, { expose }) {
-  expose();
+  setup(__props, { expose: __expose }) {
+  __expose();
 
 _useCssVars(_ctx => ({
   \\"xxxxxxxx-color\\": (color),

commit 3703006a6888ee55e39a477b0f59c1065adbb2d7

fix(compiler-sfc): fix defineExpose() codegen regression from #7949

packages/compiler-sfc/src/compileScript.ts
@@ -1252,7 +1252,7 @@ export function compileScript(
         s.overwrite(
           callee.start! + startOffset,
           callee.end! + startOffset,
-          'expose'
+          '__expose'
         )
       }
     }

packages/compiler-sfc/__tests__/__snapshots__/compileScript.spec.ts.snap
@@ -645,7 +645,7 @@ exports[`SFC compile <script setup> > defineExpose() 1`] = `
 "export default {
   setup(__props, { expose: __expose }) {
 
-expose({ foo: 123 })
+__expose({ foo: 123 })
 
 return {  }
 }
@@ -1319,7 +1319,7 @@ exports[`SFC compile <script setup> > inlineTemplate mode > with defineExpose() 
   setup(__props, { expose: __expose }) {
 
         const count = ref(0)
-        expose({ count })
+        __expose({ count })
         
 return () => {}
 }

packages/compiler-sfc/__tests__/compileScript.spec.ts
@@ -276,7 +276,7 @@ defineExpose({ foo: 123 })
     // should generate correct setup signature
     expect(content).toMatch(`setup(__props, { expose: __expose }) {`)
     // should replace callee
-    expect(content).toMatch(/\bexpose\(\{ foo: 123 \}\)/)
+    expect(content).toMatch(/\b__expose\(\{ foo: 123 \}\)/)
   })
 
   test('<script> after <script setup> the script content not end with `\\n`', () => {